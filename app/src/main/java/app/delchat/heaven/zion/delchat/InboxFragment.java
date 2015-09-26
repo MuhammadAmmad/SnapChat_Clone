799ba5eaa120b04ef4546bdb9e534247c0b8bc61
Ankit <iankit17@gmail.com>
2015-09-26 17:45:52 +0530

    Fragment Updated

---
+++ app/src/main/java/app/delchat/heaven/zion/delchat/InboxFragment.java
@@ -0,0 +1,21 @@
+package app.delchat.heaven.zion.delchat;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Zion on 26/09/15.
+ */
+public class InboxFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+}
