83e6e6afc12f04a244b4ac01bc3a3145d4ccf3e7
Ankit <iankit17@gmail.com>
2015-09-29 18:34:24 +0530

    Added Camera to Menu

--- app/src/main/java/app/delchat/heaven/zion/delchat/EditFriendsActivity.java
+++ app/src/main/java/app/delchat/heaven/zion/delchat/EditFriendsActivity.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -97,13 +96,6 @@ public class EditFriendsActivity extends ListActivity {
                 }
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_friends, menu);
-        return true;
     }
 
     @Override
